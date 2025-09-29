589674d22006f3abb2d1d33b961cc8f4ca29db22
Mario Ribe Herrero <124402943+MarioRibeHerrero@users.noreply.github.com>
2025-09-29 12:36:26 +0200

    Revert "Add toggle system for project categories"
    This reverts commit c09c1592bfefae2213ce4f55662e85601ca62ced.

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,9 @@ const Header = () => {
             </div>
             <ul className="nav-links">
               <li><a href="#about">About</a></li>
-              <li><a href="#projects">Proyectos</a></li>
+              <li><a href="#web">Web</a></li>
+              <li><a href="#mobile">Mobile</a></li>
+              <li><a href="#design">Design</a></li>
               <li><a href="#contact">Contact</a></li>
             </ul>
           </div>
