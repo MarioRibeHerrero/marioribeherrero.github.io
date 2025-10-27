f14e41ddec4527c7477018e5710b3288403bb16c
Mario Ribe Herrero <124402943+MarioRibeHerrero@users.noreply.github.com>
2025-10-27 16:27:06 +0100

    WIp

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
+  plugins: [tailwindcss(), react()],
+  base: '/marioribeherrero.github.io/',
 })
+
 
 
