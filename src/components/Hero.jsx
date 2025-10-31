25f177b41e07a3acfc943e91ad7feac29f7a7821
Mario Ribe Herrero <124402943+MarioRibeHerrero@users.noreply.github.com>
2025-10-31 12:07:05 +0100

    WIP

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -67,10 +67,10 @@ const Hero = () => {
         <div className="hidden lg:block">
           <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
             <img
-              src="/images/"
+              src="/images/Combined.png"
               width={656}
               height={800}
-              alt="Henry Clark"
+              alt="Combined"
               className="w-full"
             />
           </figure>
